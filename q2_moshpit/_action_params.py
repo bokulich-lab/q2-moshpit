a49642de5adc9b85949d13317b2b7de4f232a102
Michal Ziemski <mziemski@ethz.ch>
2022-04-27 11:21:42 +0200

    add plugin integration

---
+++ q2_moshpit/_action_params.py
@@ -0,0 +1,48 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2022, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from qiime2.core.type import Bool, Int, Range, Float
+
+checkm_params = {
+    'reduced_tree': Bool,
+    'unique': Int % Range(1, None),
+    'multi': Int % Range(1, None),
+    'force_domain': Bool,
+    'no_refinement': Bool,
+    'individual_markers': Bool,
+    'skip_adj_correction': Bool,
+    'skip_pseudogene_correction': Bool,
+    'aai_strain': Float % Range(0, 1),
+    'ignore_thresholds': Bool,
+    'e_value': Float % Range(0, 1),
+    'length': Float % Range(0, 1),
+    'threads': Int % Range(0, None)
+}
+
+checkm_param_descriptions = {
+    'reduced_tree': 'Use reduced tree (requires <16GB of memory) for '
+                    'determining lineage of each bin.',
+    'unique': 'Minimum number of unique phylogenetic markers required to use '
+              'lineage-specific marker set. Default: 10.',
+    'multi': 'Maximum number of multi-copy phylogenetic markers before '
+             'defaulting to domain-level marker set. Default: 10.',
+    'force_domain': 'Use domain-level sets for all bins.',
+    'no_refinement': 'Do not perform lineage-specific marker set refinement.',
+    'individual_markers': 'Treat marker as independent '
+                          '(i.e., ignore co-located set structure).',
+    'skip_adj_correction': 'Do not exclude adjacent marker genes when '
+                           'estimating contamination.',
+    'skip_pseudogene_correction': 'Skip identification and filtering '
+                                  'of pseudogenes.',
+    'aai_strain': 'AAI threshold used to identify strain heterogeneity. '
+                  'Default: 0.9.',
+    'ignore_thresholds': 'Ignore model-specific score thresholds.',
+    'e_value': 'E-value cut off. Default: 1e-10.',
+    'length': 'Percent overlap between target and query. Default: 0.7.',
+    'threads': 'Number of threads. Default: 1.'
+}

d65409fa1ef628c4dc160399a620d31783baf122
Michal Ziemski <mziemski@ethz.ch>
2022-11-16 17:06:42 +0100

    More changes

--- q2_moshpit/kraken2/kraken2.py
+++ q2_moshpit/kraken2/kraken2.py
@@ -11,30 +11,115 @@ import subprocess
 import tempfile
 import time
 from copy import deepcopy
+from dataclasses import dataclass, field, make_dataclass
 from functools import reduce
+from typing import Union
 
-import numpy as np
 import pandas as pd
 import requests
 
 from q2_moshpit._utils import _process_common_input_params, run_command
 from q2_types_genomics.per_sample_data import MultiMAGSequencesDirFmt
-from q2_sapienns._metaphlan import metaphlan_taxon
 
 from q2_moshpit.kraken2.utils import _process_kraken2_arg
 
 
-def _parse_kraken2_report(report_fp: str, sample_name: str, bin_name: str) -> pd.DataFrame:
+RANKS = {x: y for x, y in zip('dpcofgs', range(7))}
+LEVELS = {x: y for x, y in zip(range(7), 'dpcofgs')}
+
+@dataclass
+class Node:
+    rank: str
+    name: str
+    count: int
+    level: int
+    parent_taxonomy: str = None
+    taxonomy: str = None
+    children: Union[list, None] = field(default_factory=list)
+
+    def __post_init__(self):
+        self.taxonomy = f'{self.rank}__{self.name}'
+
+# prepare taxonomic data classes
+LEVEL_CLASSES = []
+for i in range(7):
+    base_cls = (Node,) if i == 0 else (LEVEL_CLASSES[i-1],)
+    LEVEL_CLASSES.append(make_dataclass(
+        cls_name=f'L{i}',
+        fields=[],
+        bases=base_cls
+    ))
+
+
+def _find_parent(all_levels: dict, parent_rank: str, parent_name: str):
+    parent_candidates = [
+        x for x in all_levels[parent_rank] if x.name == parent_name
+    ]
+    return parent_candidates[0]
+
+
+def _parse_kraken2_report(
+        report_fp: str, sample_name: str, bin_name: str
+) -> pd.DataFrame:
     with open(report_fp, 'r') as r:
         lines = r.readlines()
 
     all_taxonomies = {}
+    levels = {x: [] for x in 'dpcofgs'}
     for line in lines:
         line = line.split('\t')
         taxonomy, count = line[0], int(line[1].strip())
         all_taxonomies[taxonomy] = {f'{sample_name}/{bin_name}': count}
 
-    return pd.DataFrame.from_dict(all_taxonomies, orient='index')
+        line = [tuple(x.split('__')) for x in line[0].split('|')]
+        for i, (rank, name) in enumerate(line):
+            existing_ranks = [node.name for node in levels[rank]]
+
+            # does it exist already?
+            if name not in existing_ranks:
+                current_level = RANKS[rank]
+                current_node = LEVEL_CLASSES[current_level](rank=rank, name=name, count=count, level=current_level)
+                # add the node to the list of ranks
+                levels[rank].append(current_node)
+                if rank != 'd':
+                    (parent_rank, parent_name) = line[i - 1]
+                    parent = _find_parent(levels, parent_rank, parent_name)
+                    # add the node to the list of children of a higher rank
+                    parent.children.append(current_node)
+                    # add the parent to the current node
+                    current_node.parent_taxonomy = parent
+
+    for _l in range(5, -1, -1):
+        current_rank = LEVELS[_l]
+        for node in levels[current_rank]:
+            child_rank, child_level = LEVELS[_l + 1], _l + 1
+            children_counts = sum([x.count for x in node.children])
+            if children_counts < node.count:
+                unclassified_node = LEVEL_CLASSES[child_level](
+                    rank=child_rank, name='unclassified',
+                    count=node.count - children_counts, level=child_level,
+                    children=None, parent_taxonomy=node)
+                node.children.append(unclassified_node)
+                levels[child_rank].append(unclassified_node)
+
+    # expand taxonomies
+    for _l in range(7):
+        current_rank = LEVELS[_l]
+        for node in levels[current_rank]:
+            if _l > 0:
+                node.taxonomy = f'{node.parent_taxonomy.taxonomy};{node.taxonomy}'
+
+    # find childless nodes
+    features = []
+    for _, taxonomies in levels.items():
+        childless = [x for x in taxonomies if not x.children]
+        features.extend(childless)
+
+    abundances = {x.taxonomy: x.count for x in features}
+    abundances = pd.DataFrame.from_dict(
+        abundances, orient='index', columns=[f'{sample_name}/{bin_name}']
+    )
+    return abundances
 
 
 def _fetch_tax_id(taxon: str) -> int:
@@ -87,27 +172,6 @@ def _process_kraken2_reports(reports: dict) -> pd.DataFrame:
         ), abundances
     )
 
-    # find all levels from all samples
-    all_levels = []
-    for taxonomy in df_merged.index.tolist():
-        for level in taxonomy.split('|'):
-            all_levels.append(level) if level not in all_levels else False
-
-    # fetch IDs from NCBI
-    # levels_with_ids = {
-    #     level: _fetch_tax_id(level.split('__')[-1]) for level in all_levels
-    # }
-    levels_with_ids = {
-        level: _id for _id, level in enumerate(sorted(all_levels))
-    }
-
-    # assign NCBI IDs
-    all_taxonomies = []
-    for taxonomy in df_merged.index:
-        _id = list(map(lambda x: str(levels_with_ids[x]), taxonomy.split('|')))
-        all_taxonomies.append('|'.join(_id))
-
-    df_merged['NCBI_tax_id'] = all_taxonomies
     df_merged.index.name = 'feature-id'
 
     return df_merged
@@ -139,6 +203,19 @@ def _process_kraken2_outputs(reports: dict) -> pd.DataFrame:
     return pd.concat(contig_info_all, axis=0)
 
 
+def _fill_missing_levels(
+        taxon: str, sep: str =';',
+        fill_with: str = 'unclassified',
+        prepend: bool =True
+) -> str:
+    ranks = 'dpcofgs'
+    taxon = taxon.split(sep)
+    for i in range(len(taxon), 7):
+        level = f'{ranks[i]}__{fill_with}' if prepend else fill_with
+        taxon.append(level)
+    return sep.join(taxon)
+
+
 def _classify_kraken(manifest, common_args) -> (pd.DataFrame, pd.DataFrame):
     base_cmd = ["kraken2", *common_args]
     kraken2_reports = {}
@@ -168,17 +245,26 @@ def _classify_kraken(manifest, common_args) -> (pd.DataFrame, pd.DataFrame):
                 "stdout and stderr to learn more."
             )
         results_df = _process_kraken2_reports(kraken2_reports)
-        #contigs_to_taxons = _process_kraken2_outputs(kraken2_reports)
 
+        # fill missing levels
+        results_df.index = results_df.index.map(_fill_missing_levels)
+
+        #contigs_to_taxons = _process_kraken2_outputs(kraken2_reports)
         #grouped = contigs_to_taxons.groupby(['sample', 'taxon']).sum()
 
-        # TODO: make the level configurable?
-        (table, taxonomy) = metaphlan_taxon(
-            stratified_table=results_df, level=7
-        )
-        table.fillna(0, inplace=True)
+        results_df.fillna(0, inplace=True)
 
-    return table, taxonomy
+        # create "fake" taxonomy for now
+        # should we use the lowest level's NCBI ID here?
+        taxonomy = pd.DataFrame(
+            results_df.index.tolist(), index=results_df.index, columns=['Taxon']
+        )
+        taxonomy.index.name = 'Feature ID'
+        taxonomy['Taxon'] = taxonomy['Taxon'].apply(
+            lambda x: x.replace('|', ';')
+        )
+
+    return results_df.T, taxonomy
 
 
 def classify_kraken(

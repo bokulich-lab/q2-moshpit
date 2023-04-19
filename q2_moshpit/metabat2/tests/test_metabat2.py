e63c37984953b16475dfc3a9ea280fdfb5ae1fd5
Keegan Evans <keegan.evans@gmail.com>
2023-04-19 13:54:28 -0700

    TEST: Updating metabat2 tests to be compatible w/ @greg-caporaso 's suffix based sample name extraction pr

--- q2_moshpit/metabat2/tests/test_metabat2.py
+++ q2_moshpit/metabat2/tests/test_metabat2.py
@@ -39,30 +39,30 @@ class TestMetabat2(TestPluginBase):
         self.assertEqual(exp, obs)
 
     def test_assert_samples_ok(self):
-        contigs = ['/a/b/s1_tigs.fa', '/a/b/s3_tigs.fa', '/a/b/s2_tigs.fa']
-        maps = ['/a/b/s3_aln.bam', '/a/b/s2_aln.bam', '/a/b/s1_aln.bam']
+        contigs = ['/a/b/s1_contigs.fa', '/a/b/s3_contigs.fa', '/a/b/s2_contigs.fa']
+        maps = ['/a/b/s3_alignment.bam', '/a/b/s2_alignment.bam', '/a/b/s1_alignment.bam']
 
         obs_samples = _assert_samples(contigs, maps)
         exp_samples = {
-            's1': {'contigs': '/a/b/s1_tigs.fa', 'map': '/a/b/s1_aln.bam'},
-            's2': {'contigs': '/a/b/s2_tigs.fa', 'map': '/a/b/s2_aln.bam'},
-            's3': {'contigs': '/a/b/s3_tigs.fa', 'map': '/a/b/s3_aln.bam'}
+            's1': {'contigs': '/a/b/s1_contigs.fa', 'map': '/a/b/s1_alignment.bam'},
+            's2': {'contigs': '/a/b/s2_contigs.fa', 'map': '/a/b/s2_alignment.bam'},
+            's3': {'contigs': '/a/b/s3_contigs.fa', 'map': '/a/b/s3_alignment.bam'}
         }
         self.assertDictEqual(exp_samples, obs_samples)
 
     def test_assert_samples_uneven(self):
-        contigs = ['/a/b/s1_tigs.fa', '/a/b/s3_tigs.fa']
-        maps = ['/a/b/s3_aln.bam', '/a/b/s2_aln.bam', '/a/b/s1_aln.bam']
+        contigs = ['/a/b/s1_contigs.fa', '/a/b/s3_contigs.fa']
+        maps = ['/a/b/s3_alignment.bam', '/a/b/s2_alignment.bam', '/a/b/s1_alignment.bam']
 
         with self.assertRaisesRegex(
                 Exception,
-                'contigs for samples: s1,s3 but maps for samples: s1,s2,s3'
+                'Contigs for samples: s1,s3 but maps for samples: s1,s2,s3'
         ):
             _assert_samples(contigs, maps)
 
     def test_assert_samples_non_matching(self):
-        contigs = ['/a/b/s1_tigs.fa', '/a/b/s4_tigs.fa', '/a/b/s2_tigs.fa']
-        maps = ['/a/b/s3_aln.bam', '/a/b/s2_aln.bam', '/a/b/s1_aln.bam']
+        contigs = ['/a/b/s1_contigs.fa', '/a/b/s4_contigs.fa', '/a/b/s2_contigs.fa']
+        maps = ['/a/b/s3_alignment.bam', '/a/b/s2_alignment.bam', '/a/b/s1_alignment.bam']
 
         with self.assertRaisesRegex(
                 Exception,

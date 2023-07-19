da47fbc3c2f02b2328b1da20eefaa9d02e56cd87
Michal Ziemski <mziemski@ethz.ch>
2023-07-19 10:25:25 +0200

    Update dereplication to use MAG UUIDs

--- q2_moshpit/dereplication/derep.py
+++ q2_moshpit/dereplication/derep.py
@@ -83,6 +83,7 @@ def find_similar_bins_fcluster(
 
     return list(clusters.values())
 
+
 def get_bin_lengths(mags: MultiMAGSequencesDirFmt) -> pd.Series:
     """
     Calculates the length of each bin in a MultiMAGSequencesDirFmt object.
@@ -100,13 +101,13 @@ def get_bin_lengths(mags: MultiMAGSequencesDirFmt) -> pd.Series:
             tot += len(_seq)
         bin_lengths[str(path)] = tot
     ser = pd.Series(bin_lengths, name="length")
-    ser.index = ser.index.map(lambda x: x.replace(".fasta", "").replace("/", "_"))
+    ser.index = ser.index.map(lambda x: x.replace(".fasta", "").split("/")[-1])
     return ser
 
 
 def remap_bins(
     bin_clusters: List[List[str]], longest_bins: List[int], distances: pd.DataFrame
-) -> Dict[str, Tuple[str, str]]:
+) -> Dict[str, str]:
     """
     Maps duplicate bins to a single dereplicated bin and assigns new IDs to the unique bins.
 
@@ -120,18 +121,17 @@ def remap_bins(
 
     Notes:
         The output will look like:
-            {'sample1_bin1': ('sample1_bin1', 'mag5'),
-             'sample1_bin2': ('sample1_bin2', 'mag2'),
-             'sample2_bin1': ('sample2_bin1', 'mag8'),
-             'sample2_bin2': ('sample2_bin2', 'mag1'),
-             'sample2_bin3': ('sample2_bin3', 'mag6'),
-             'sample2_bin4': ('sample2_bin4', 'mag3'),
-             'sample2_bin5': ('sample2_bin5', 'mag7'),
-             'sample3_bin1': ('sample2_bin4', 'mag3'),
-             'sample3_bin2': ('sample3_bin2', 'mag4'),
-             'sample3_bin3': ('sample1_bin1', 'mag5')}
-        meaning that mag5 is was assigned to both, sample1_bin1 and sample3_bin3, but sample1_bin1 will be used
-        for both, as it was longer.
+            {'sample1_bin1': 'mag5',
+             'sample1_bin2': 'mag2',
+             'sample2_bin1': 'mag8',
+             'sample2_bin2': 'mag1',
+             'sample2_bin3': 'mag6',
+             'sample2_bin4': 'mag3',
+             'sample2_bin5': 'mag7',
+             'sample3_bin1': 'mag3',
+             'sample3_bin2': 'mag4',
+             'sample3_bin3': 'mag5'}
+        meaning that mag5 was assigned to both, sample1_bin1 and sample3_bin3.
     """
     final_bins = {}
     for i, similar_bins in enumerate(bin_clusters):
@@ -141,28 +141,20 @@ def remap_bins(
         if bin not in final_bins:
             final_bins[bin] = bin
 
-    # find unique bins and assign them a new ID
-    unique_bins = list(set(final_bins.values()))
-    new_ids = [f"mag{i}" for i in range(1, len(unique_bins) + 1)]
-
-    # assign new IDs to old bins
-    for old_id, new_id in zip(unique_bins, new_ids):
-        for bin, new_bin in final_bins.items():
-            if new_bin == old_id:
-                final_bins[bin] = (old_id, new_id)
-
     return final_bins
 
 
 def reassign_bins_to_samples(
-    final_bins: Dict[str, Tuple[str, str]]
+    final_bins: Dict[str, str], manifest: pd.DataFrame
 ) -> Dict[str, Dict[str, int]]:
     """
     Assigns bins to samples based on the final bin mapping.
 
     Args:
-        final_bins (dict): A dictionary where the keys are the original bin names and the values are tuples of the
-                            old and new bin IDs.
+        final_bins (dict): A dictionary where the keys are the original bin names and the values are the
+                            new bin IDs.
+        manifest (pd.DataFrame): Manifest of the original sample set - required to recover information
+                                    about original sample IDs.
 
     Returns:
         A dictionary where the keys are sample IDs and the values are dictionaries where the keys are MAG IDs and
@@ -174,21 +166,24 @@ def reassign_bins_to_samples(
              'sample2': {'mag1': 1, 'mag2': 0, 'mag3': 1, 'mag4': 0, 'mag5': 0, 'mag6': 1, 'mag7': 1, 'mag8': 1},),
              'sample3': {'mag1': 0, 'mag2': 0, 'mag3': 1, 'mag4': 1, 'mag5': 1, 'mag6': 0, 'mag7': 0, 'mag8': 0},)}.
     """
-    all_samples = set(
-        [sample_bin.rsplit("_", maxsplit=1)[0] for sample_bin in final_bins.keys()]
-    )
-    all_derep_mags = set([mag_id for _, (_, mag_id) in final_bins.items()])
-    samples_to_bins = {key: {mag: 0 for mag in all_derep_mags} for key in all_samples}
+    all_samples = manifest.copy(deep=True).reset_index().replace({"mag-id": final_bins})
+    all_derep_mags = set([mag_id for _, mag_id in final_bins.items()])
 
-    for samp_bin1, (samp_bin_2, mag_id) in final_bins.items():
-        sample_id = samp_bin1.rsplit("_", maxsplit=1)[0]
+    samples_to_bins = {
+        key: {mag: 0 for mag in all_derep_mags}
+        for key in set(all_samples["sample-id"])
+    }
+
+    for i, row in all_samples.iterrows():
+        sample_id = row["sample-id"]
+        mag_id = row["mag-id"]
         samples_to_bins[sample_id][mag_id] += 1
 
     return samples_to_bins
 
 
 def write_unique_bins(
-    all_bins: MultiMAGSequencesDirFmt, bins_remapped: Dict[str, Tuple[str, str]]
+    all_bins: MultiMAGSequencesDirFmt, bins_remapped: Dict[str, str]
 ) -> MAGSequencesDirFmt:
     """
     Writes the unique bins to a new MAGSequencesDirFmt object based on the final bin mapping.
@@ -203,11 +198,11 @@ def write_unique_bins(
     """
     derep_bins = MAGSequencesDirFmt()
     manifest = all_bins.manifest.view(pd.DataFrame)
-    for sample_bin, (old_bin_id, new_bin_id) in bins_remapped.items():
-        sample, bin = old_bin_id.rsplit("_", maxsplit=1)
+    manifest.index = manifest.index.droplevel(0)
+    for old_bin_id, new_bin_id in bins_remapped.items():
         dst_bin = os.path.join(str(derep_bins), f"{new_bin_id}.fasta")
         if not os.path.isfile(dst_bin):
-            src_bin = manifest.loc[(sample, bin), "filename"]
+            src_bin = manifest.loc[new_bin_id, "filename"]
             shutil.copy(src_bin, dst_bin)
     return derep_bins
 
@@ -246,14 +241,16 @@ def dereplicate_mags(
     bin_lengths = get_bin_lengths(mags)
     longest_bins = [bin_lengths[ids].idxmax() for ids in bin_clusters]
 
-    # generate a map between the original bins and the dereplicated bins (and their new IDs)
+    # generate a map between the original bins and the dereplicated bins
     final_bins = remap_bins(bin_clusters, longest_bins, distances)
 
     # generate dereplicated bin sequences
     unique_bin_seqs = write_unique_bins(mags, final_bins)
 
     # generate a presence-absence table
-    unique_bins_per_sample = reassign_bins_to_samples(final_bins)
+    unique_bins_per_sample = reassign_bins_to_samples(
+        final_bins, mags.manifest.view(pd.DataFrame)
+    )
     presence_absence = generate_pa_table(unique_bins_per_sample)
 
     return unique_bin_seqs, presence_absence

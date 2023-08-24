422063f4c1f3fa415332e7c9306c43bc3d63dbcd
Santiago Castro Dau <sanntiago5@gmail.com>
2023-08-24 15:02:51 +0200

    begining test suit for busco

---
+++ q2_moshpit/tests/test_busco.py
@@ -0,0 +1,10 @@
+"""
+Unit test for BUSCO wrapper.
+"""
+
+# TODO: Test `_parse_busco_params`
+# TODO: Test `_zip_busco_plots`
+# TODO: Test `_draw_busco_plots`
+# TODO: Test `_run_busco`
+# TODO: Test `_draw_busco_plots_for_render`
+# TODO: Integration test busco.

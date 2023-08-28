8cc40e43e4bef1cc2a82af60534d5ae42c91e50c
Santiago Castro Dau <sanntiago5@gmail.com>
2023-08-28 12:09:55 +0200

    started developing the test suite for busco

--- q2_moshpit/busco/tests/conftest.py
+++ q2_moshpit/busco/tests/conftest.py
@@ -1,24 +1,32 @@
 import pytest
 from q2_types_genomics.per_sample_data._format import MultiMAGSequencesDirFmt
+from zipfile import ZipFile
+import os
 
 
 @pytest.fixture(scope="session")
-def MAGS_test_data():
+def MAGS_test_data(tmp_path_factory):
     """
-    Fixture for BUSCO testing. It unzips a directoy with some test data and initializes a
-    MultiMAGSequencesDirFmt which gets passed to the tests.
+    Fixture for BUSCO testing. It unzips a directoy with some test data
+    and initializes a MultiMAGSequencesDirFmt which gets passed to the tests.
 
     Returns:
         test_data (MultiMAGSequencesDirFmt): object with the data for tests
     """
 
-    # TODO: make temp dir for test data
+    # Get temporary directory
+    tmp_path = tmp_path_factory.mktemp("busco_pytest_tmpdir")
 
-    # TODO: expand zip into temp dir
+    # Unzip files into temp dir
+    with ZipFile("./data/test_data.zip", "r") as f:
+        f.extractall(tmp_path)
 
-    # TODO: Initialize object with downloaded data
+    # NOTE: tmp_path_factory is a fixed name defined in pytest which is
+    # passed on to your testfunction if you have it as an argument name.
+
+    # Initialize object with path to unziped files
     test_data = MultiMAGSequencesDirFmt(
-        path="/Users/santiago/moshpit_tutorial/0a2e152b-c5bb-4f61-ad75-d31d7a9b5ab6",
+        path=os.path.join(tmp_path),
         mode="r",
     )
 
--- q2_moshpit/busco/tests/test_busco.py
+++ q2_moshpit/busco/tests/test_busco.py
@@ -1,8 +1,28 @@
 """
 Unit test for BUSCO wrapper.
 """
+import os
+from ..busco import _parse_busco_params
 
-# TODO: Test `_parse_busco_params`
+
+# Test that fixtrue is working.
+def test_tmp_path_factory_fixtrue(MAGS_test_data):
+    assert os.path.exists(MAGS_test_data.path)
+
+
+# Test `_parse_busco_params`
+def test_parse_busco_params_1():
+    observed = _parse_busco_params("auto_lineage", True)
+    expected = ["--auto-lineage"]
+    assert set(observed) == set(expected)
+
+
+def test_parse_busco_params_2():
+    observed = _parse_busco_params("evalue", 0.66)
+    expected = ["--evalue", str(0.66)]
+    assert set(observed) == set(expected)
+
+
 # TODO: Test `_zip_busco_plots`
 # TODO: Test `_draw_busco_plots`
 # TODO: Test `_run_busco`

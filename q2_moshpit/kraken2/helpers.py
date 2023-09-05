36b73f356f9a14ed443599cfb8bee53134a8ea8e
Oddant1 <oddant1@hotmail.com>
2023-09-05 15:16:03 -0700

    Start adding collate helpers

---
+++ q2_moshpit/kraken2/helpers.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2022-2023, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from q2_types_genomics.kraken2 import (
+    Kraken2ReportDirectoryFormat,
+    Kraken2OutputDirectoryFormat
+)
+
+
+def collate_kraken2_reports(kraken2_reports: Kraken2ReportDirectoryFormat) \
+        -> Kraken2ReportDirectoryFormat:
+    pass
+
+
+def collate_kraken2_outputs(kraken2_outputs: Kraken2OutputDirectoryFormat) \
+        -> Kraken2OutputDirectoryFormat:
+    pass

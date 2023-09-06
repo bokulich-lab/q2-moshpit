dcfa1afc5c14991b4665a25288e17e22e787bdde
Oddant1 <oddant1@hotmail.com>
2023-09-06 14:39:19 -0700

    Refactor to reflect how data actually looks at this point

--- q2_moshpit/kraken2/helpers.py
+++ q2_moshpit/kraken2/helpers.py
@@ -16,10 +16,6 @@ from q2_types_genomics.kraken2 import (
 )
 
 
-_COLLATE_ERROR_BASE = "This most likely means you are using MAGs which are " \
-                      "not supported here at this time."
-
-
 def collate_kraken2_reports(kraken2_reports: Kraken2ReportDirectoryFormat) \
         -> Kraken2ReportDirectoryFormat:
     collated_kraken2_reports = Kraken2ReportDirectoryFormat()
@@ -39,22 +35,9 @@ def _collate_kraken_tsvs(kraken2_results, kraken_type, output):
 
     for kraken2_result in kraken2_results:
         for fp in kraken2_result.path.iterdir():
-            sample_id = os.path.basename(fp)
-            sample_fps = os.listdir(fp)
-
-            if len(sample_fps) > 0:
-                raise ValueError(
-                    f"You have more than one {kraken_type} for the sample "
-                    f"{sample_id}. " + _COLLATE_ERROR_BASE)
-
-            sample_fp = fp / sample_fps[0]
-
-            if os.path.isdir(sample_fp):
-                raise ValueError(
-                        f"The directory for the sample id {sample_id} "
-                        "contains another directory. " + _COLLATE_ERROR_BASE)
-
-            df = pd.read_csv(sample_fp, sep='\t', header=None)
+            # fp will be "sample_id.output/report.txt/tsv" we just want the id
+            sample_id = os.path.basename(fp).split('.')[0]
+            df = pd.read_csv(fp, sep='\t', header=None)
 
             if sample_id in collated_sample_outputs:
                 collated_sample_outputs[sample_id].append(df)

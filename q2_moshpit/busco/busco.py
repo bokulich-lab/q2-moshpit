bdf5dfc05b4e5e1b042ca4454fd2979c1ce0e372
Santiago Castro Dau <sanntiago5@gmail.com>
2023-09-21 16:02:22 +0200

    Bunch of work on reformatting the tests. Some parallel work on the source code.

--- q2_moshpit/busco/busco.py
+++ q2_moshpit/busco/busco.py
@@ -8,19 +8,13 @@
 
 
 import os
-import json
 import tempfile
-import q2templates
-import pandas as pd
-from shutil import copytree
-from .utils import (
+import q2_moshpit.busco.utils
+from q2_moshpit.busco.utils import (
     _parse_busco_params,
-    _draw_busco_plots_for_render,
-    _run_busco,
-    _draw_busco_plots,
-    _zip_busco_plots,
+    _render_html,
 )
-from .._utils import _process_common_input_params
+from q2_moshpit._utils import _process_common_input_params
 from typing import List
 from q2_types_genomics.per_sample_data._format import MultiMAGSequencesDirFmt
 
@@ -28,40 +22,40 @@ from q2_types_genomics.per_sample_data._format import MultiMAGSequencesDirFmt
 def busco(
     output_dir: str,
     bins: MultiMAGSequencesDirFmt,
-    mode: str = None,
+    mode: str = "genome",
     lineage_dataset: str = None,
-    augustus: bool = None,
+    augustus: bool = False,
     augustus_parameters: str = None,
     augustus_species: str = None,
-    auto_lineage: bool = None,
-    auto_lineage_euk: bool = None,
-    auto_lineage_prok: bool = None,
-    cpu: int = None,
+    auto_lineage: bool = False,
+    auto_lineage_euk: bool = False,
+    auto_lineage_prok: bool = False,
+    cpu: int = 1,
     config: str = None,
-    contig_break: int = None,
+    contig_break: int = 10,
     datasets_version: str = None,
     download: List[str] = None,
     download_base_url: str = None,
     download_path: str = None,
-    evalue: float = None,
-    force: bool = None,
-    limit: int = None,
-    help: bool = None,
-    list_datasets: bool = None,
-    long: bool = None,
+    evalue: float = 1e-03,
+    force: bool = False,
+    limit: int = 3,
+    help: bool = False,
+    list_datasets: bool = False,
+    long: bool = False,
     metaeuk_parameters: str = None,
     metaeuk_rerun_parameters: str = None,
-    miniprot: bool = None,
-    offline: bool = None,
-    quiet: bool = None,
-    restart: bool = None,
-    scaffold_composition: bool = None,
-    tar: bool = None,
-    update_data: bool = None,
-    version: bool = None,
+    miniprot: bool = False,
+    offline: bool = False,
+    quiet: bool = False,
+    restart: bool = False,
+    scaffold_composition: bool = False,
+    tar: bool = False,
+    update_data: bool = False,
+    version: bool = False,
 ) -> None:
     """
-    qiime2 vizualization for the BUSCO assessment tool
+    qiime2 visualization for the BUSCO assessment tool
     <https://busco.ezlab.org/>.
 
     Args:
@@ -69,18 +63,18 @@ def busco(
 
     Output:
         plots.zip: zip file containing all of the busco plots
-        busco_output: all busco outputfiles
+        busco_output: all busco output files
         qiime_html: html for rendering the output plots
     """
 
-    # Create dictionary with local varaibles
-    # (kwards passed to the function or their defaults) excluding
+    # Create dictionary with local variables
+    # (kwargs passed to the function or their defaults) excluding
     # "output_dir" and "bins"
     kwargs = {
         k: v for k, v in locals().items() if k not in ["output_dir", "bins"]
     }
 
-    # Filter out all kwards that are None, False or 0.0
+    # Filter out all kwargs that are None, False or 0.0
     common_args = _process_common_input_params(
         processing_func=_parse_busco_params, params=kwargs
     )
@@ -88,84 +82,40 @@ def busco(
     # Creates output directory with path 'tmp'
     with tempfile.TemporaryDirectory() as tmp:
         # Run busco for every sample. Returns dictionary to report files.
-        results_dir = os.path.join(tmp, "busco_output")
-        path_to_run_summeries = _run_busco(
-            output_dir=results_dir, mags=bins, params=common_args
+        # Result NOT included in final output
+        busco_results_dir = os.path.join(tmp, "busco_output")
+        path_to_run_summaries = q2_moshpit.busco.utils._run_busco(
+            output_dir=busco_results_dir,
+            mags=bins,
+            params=common_args,
         )
 
         # Collect result for each sample and save to file.
-        all_summeries_list = []
-        for sample_id, path_to_summary in path_to_run_summeries.items():
-            df = pd.read_csv(filepath_or_buffer=path_to_summary, sep="\t")
-            df["sample_id"] = sample_id
-            all_summeries_list.append(df)
-
-        # Concatenate
-        all_summeries_df = pd.concat(all_summeries_list)
-
-        # Save to file
-        all_summeries_path = os.path.join(
-            output_dir, "all_batch_summeries.csv"
-            )
-        all_summeries_df.to_csv(all_summeries_path, index=False)
+        # Result included in final output (file for download)
+        all_summaries_path = os.path.join(
+            output_dir, "all_batch_summaries.csv"
+        )
+        all_summaries_df = q2_moshpit.busco.utils._collect_summaries_and_save(
+            all_summaries_path=all_summaries_path,
+            path_to_run_summaries=path_to_run_summaries,
+        )
 
         # Draw BUSCO plots for all samples
+        # Result NOT included in final output
         plots_dir = os.path.join(tmp, "plots")
-        paths_to_plots = _draw_busco_plots(
-            path_to_run_summeries=path_to_run_summeries, output_dir=plots_dir
+        paths_to_plots = q2_moshpit.busco.utils._draw_busco_plots(
+            path_to_run_summaries=path_to_run_summaries,
+            plots_dir=plots_dir
         )
 
         # Zip graphs for user download
+        # Result included in final output (file for download)
         zip_name = os.path.join(output_dir, "busco_plots.zip")
-        _zip_busco_plots(paths_to_plots=paths_to_plots, zip_path=zip_name)
+        q2_moshpit.busco.utils._zip_busco_plots(
+            paths_to_plots=paths_to_plots,
+            zip_path=zip_name
+        )
 
         # Render qiime html report
-        # Prepare context for jinja2 template
-        context = {
-            "tabs": [{"title": "BUSCO Results", "url": "index.html"}],
-            "samples": json.dumps(list(path_to_run_summeries.keys())),
-            "vega_plots_overview": _draw_busco_plots_for_render(
-                all_summeries_df,
-                width=600,
-                height=18,
-                titleFontSize=20,
-                labelFontSize=17,
-            ),
-        }
-
-        # Copy BUSCO results from tmp dir to output_dir
-        moshpit_path = os.path.dirname(  # Path to parent dir, q2_moshpit
-            os.path.dirname(  # Path to parent dir, e.g. busco
-                os.path.abspath(__file__)  # Path to this file
-            )
-        )
-        TEMPLATES = os.path.join(moshpit_path, "assets")
-        index = os.path.join(TEMPLATES, "busco", "index.html")
-        copytree(
-            src=os.path.join(TEMPLATES, "busco"),
-            dst=output_dir,
-            dirs_exist_ok=True
-        )
-        copytree(
-            src=plots_dir,
-            dst=os.path.join(output_dir, "plots"),
-            dirs_exist_ok=True
-        )
-
-        # Render
-        templates = [index]
-        q2templates.render(templates, output_dir, context=context)
-
-        # Remove unwanted files
-        # until Bootstrap 3 is replaced with v5, remove the v3 scripts as
-        # the HTML files are adjusted to work with v5
-        os.remove(
-            os.path.join(
-                output_dir, "q2templateassets", "css", "bootstrap.min.css"
-                )
-        )
-        os.remove(
-            os.path.join(
-                output_dir, "q2templateassets", "js", "bootstrap.min.js"
-                )
-        )
+        # Result included in final output
+        _render_html(output_dir, all_summaries_df)
--- q2_moshpit/busco/tests/test_busco.py
+++ q2_moshpit/busco/tests/test_busco.py
@@ -1,160 +1,353 @@
-"""
-Unit test for BUSCO wrapper.
-"""
+# ----------------------------------------------------------------------------
+# Copyright (c) 2022-2023, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 import os
-import json
+import tempfile
 import pandas as pd
-from ..busco import (
+from q2_moshpit.busco.busco import busco
+from q2_moshpit.busco.utils import (
     _parse_busco_params,
     _draw_busco_plots,
     _zip_busco_plots,
     _run_busco,
     _draw_busco_plots_for_render,
-    busco,
+    _collect_summaries_and_save,
 )
-from ..._utils import _process_common_input_params
+from unittest.mock import patch
+from qiime2.plugin.testing import TestPluginBase
+from q2_types_genomics.per_sample_data._format import MultiMAGSequencesDirFmt
 
 
-# Test that fixtrue is working.
-def test_tmp_path_factory_fixtrue(MAGS_test_data):
-    assert os.path.exists(MAGS_test_data.path)
+class TestBUSCO(TestPluginBase):
+    package = "q2_moshpit.busco.tests"
 
+    def get_dummy_mags(self):
+        """
+        Fixture for BUSCO testing. Initializes a MultiMAGSequencesDirFmt which 
+        gets passed to the tests.
 
-# Test `_parse_busco_params`
-def test_parse_busco_params_1():
-    observed = _parse_busco_params("auto_lineage", True)
-    expected = ["--auto-lineage"]
-    assert set(observed) == set(expected)
+        Returns:
+            test_data (MultiMAGSequencesDirFmt): object with the data for tests
+        """
+        # Initialize object with path to unzipped files
+        return MultiMAGSequencesDirFmt(
+            path=os.path.join(os.path.dirname(__file__), "data"),
+            mode="r",
+        )
 
+    # Test `_parse_busco_params`
+    def test_parse_busco_params_1(self):
+        observed = _parse_busco_params("auto_lineage", True)
+        expected = ["--auto-lineage"]
+        self.assertSetEqual(set(observed), set(expected))
 
-def test_parse_busco_params_2():
-    observed = _parse_busco_params("evalue", 0.66)
-    expected = ["--evalue", str(0.66)]
-    assert set(observed) == set(expected)
+    def test_parse_busco_params_2(self):
+        observed = _parse_busco_params("evalue", 0.66)
+        expected = ["--evalue", str(0.66)]
+        self.assertSetEqual(set(observed), set(expected))
 
+    def test_collect_summaries_and_save(self):
+        """
+        Test for `_collect_summaries_and_save` function.
+        Uses data stored in ./data. Checks for data frame equality.
+        """
+        with tempfile.TemporaryDirectory() as tmp_path:
+            path_to_summaries = {}
+            common_path = os.path.join(os.path.dirname(__file__), "data")
 
-def test_process_common_input_params():
-    data = {
-        "a": 0,
-        "b": 1,
-        "c": 0.0,
-        "d": 3.14,
-        "e": "",
-        "f": "Hello",
-        "g": None,
-        "h": 42,
-        "i": 0.0,
-        "j": [],
-        "k": "World",
-        "l": False,
-        "m": True,
-    }
-    observed = _process_common_input_params(_parse_busco_params, data)
-    expected = [
-        "--a",
-        "0",
-        "--b",
-        "1",
-        "--c",
-        "0.0",
-        "--d",
-        "3.14",
-        "--f",
-        "Hello",
-        "--h",
-        "42",
-        "--i",
-        "0.0",
-        "--k",
-        "World",
-        "--m",
-    ]
-    assert set(observed) == set(expected)
+            for i in range(1, 4):
+                path_to_summaries[f"sample{i}"] = os.path.join(
+                    common_path,
+                    f"batch_summary_sample{i}.txt"
+                )
 
+            observed = _collect_summaries_and_save(
+                path_to_run_summaries=path_to_summaries,
+                all_summaries_path=os.path.join(tmp_path, "aggregated.csv"),
+            )
 
-# Test `_zip_busco_plots` and `_draw_busco_plots`
-def test_zip_busco_plots(tmp_path):
-    # Create an empty dictionary to store the DataFrames
-    path_to_run_summeries = {}
+            p = os.path.join(common_path, "all_batch_summaries.csv")
+            expected = pd.read_csv(p)
+            pd.set_option('display.max_columns', None)
+            self.assertTrue(observed.equals(expected))
 
-    # Group the DataFrame by the 'sample_id' column
-    p = os.path.dirname(os.path.abspath(__file__))
-    p = os.path.join(p, "./data/all_batch_summeries.csv")
-    df = pd.read_csv(p)
-    grouped = df.groupby("sample_id")
+    # Test `_draw_busco_plots`
+    def draw_n_busco_plots(self, filename, delim):
+        """
+        Creates plot from a table containing information about
+        one or more samples. Checks for the existence of the output
+        plots.
 
-    # Iterate through the groups and store each group as a
-    # DataFrame in the dictionary
-    for sample_id, group_df in grouped:
-        path_to_df = f"{tmp_path}/{sample_id}.csv"
-        group_df.to_csv(path_to_df, sep="\t", index=False)
-        path_to_run_summeries[sample_id] = path_to_df
+        Args:
+            filename (str): name of file in ./data to construct the images
+            delim (str): delimiter of `filename`
+        """
+        # Create an empty dictionary to store the DataFrames
+        path_to_run_summaries = {}
 
-    # Draw plots
-    paths_to_plots = _draw_busco_plots(
-        path_to_run_summeries=path_to_run_summeries,
-        output_dir=os.path.join(tmp_path, "plots"),
-    )
+        # Group the DataFrame by the 'sample_id' column
+        p = os.path.join(
+            os.path.dirname(__file__),
+            f"data/{filename}"
+        )
+        df = pd.read_csv(p, delimiter=delim)
+        print(df)
+        grouped = df.groupby("sample_id")
 
-    # Zip graphs for user download
-    zip_name = os.path.join(tmp_path, "busco_plots.zip")
-    _zip_busco_plots(paths_to_plots=paths_to_plots, zip_path=zip_name)
+        # Iterate through the groups and store each group as a
+        # DataFrame in the dictionary
+        # Creates output directory with path 'tmp'
+        with tempfile.TemporaryDirectory() as tmp_path:
+            for sample_id, group_df in grouped:
+                path_to_df = f"{tmp_path}/{sample_id}.csv"
+                group_df.to_csv(path_to_df, sep="\t", index=False)
+                path_to_run_summaries[sample_id] = path_to_df
 
-    # Check for existence of file
-    assert os.path.exists(zip_name)
+            # Draw plots
+            paths_to_plots = _draw_busco_plots(
+                path_to_run_summaries=path_to_run_summaries,
+                plots_dir=os.path.join(tmp_path, "plots"),
+            )
 
+            # Check if busco plots are in fact generated
+            for _, value in paths_to_plots.items():
+                self.assertTrue(os.path.exists(value))
 
-# Test `_run_busco`
-def test_run_busco(tmp_path, MAGS_test_data):
-    # Define comand arguments and parse them
-    kwargs = {
-        "mode": "genome",
-        "lineage_dataset": "bacteria_odb10",
-        "download_path": tmp_path,
-    }
-    common_args = _process_common_input_params(
-        processing_func=_parse_busco_params, params=kwargs
-    )
+    def test_draw_busco_plots_multiple(self):
+        self.draw_n_busco_plots(
+            filename="all_batch_summaries.csv", delim=","
+        )
 
-    # Run busco and save paths to run summeries
-    path_to_run_summeries = _run_busco(
-        output_dir=os.path.join(tmp_path, "busco_output"),
-        mags=MAGS_test_data,
-        params=common_args,
-    )
+    def test_draw_busco_plots_one(self):
+        self.draw_n_busco_plots(
+            filename="batch_summary_sample1.txt", delim="\t"
+        )
 
-    # Test existence of output files
-    for _, path_to_summary in path_to_run_summeries.items():
-        assert os.path.exists(path_to_summary)
+    # Test `_draw_busco_plots_for_render`
+    def test_draw_busco_plots_for_render(self):
+        """
+        Tests function `_draw_busco_plots_for_render`.
+        Checks for dictionary equality.
+        """
+        # Load data
+        p = os.path.dirname(os.path.abspath(__file__))
+        p2 = os.path.join(p, "data/all_batch_summaries.csv")
+        all_summaries_df = pd.read_csv(p2)
 
+        # Draw plot
+        observed = _draw_busco_plots_for_render(
+            all_summaries_df,
+            width=600,
+            height=18,
+            titleFontSize=20,
+            labelFontSize=17,
+        )
 
-# Test `_draw_busco_plots_for_render`
-def test_draw_busco_plots_for_render():
-    p = os.path.dirname(os.path.abspath(__file__))
-    p = os.path.join(p, "./data/all_batch_summeries.csv")
-    all_summeries_df = pd.read_csv(p)
-    plot_as_dict = _draw_busco_plots_for_render(
-        all_summeries_df,
-        width=600,
-        height=18,
-        titleFontSize=20,
-        labelFontSize=17,
-    )
-    assert isinstance(json.loads(plot_as_dict), dict)
+        # Load expected data
+        p = os.path.join(p, "data/plot_as_dict.json")
+        with open(p, "r") as json_file:
+            expected = json_file.read()
 
+        self.assertEqual(expected, observed)
 
-# Integration test busco.
-def test_integration_busco(tmp_path, MAGS_test_data):
-    # Defin kwargs
-    kwargs = {
-        "mode": "genome",
-        "lineage_dataset": "bacteria_odb10",
-        "download_path": tmp_path,
-        "output_dir": str(tmp_path),  # Needs to be string
-    }
+    # Test `_draw_busco_plots`
+    def mock_draw_busco_plots(self, tmp_path: str, num_files: int) -> dict:
+        """
+        Mocks the generation of sample wise plots by generating
+        empty files.
 
-    # Run busco
-    busco(**kwargs, bins=MAGS_test_data)
+        Args:
+            tmp_path (str): Path where to write the empty files.
+            num_files (int): number of empty files to create, one per sample.
 
-    # Check for the existence of the html file
-    assert os.path.exists(f"{tmp_path}/index.html")
+        Returns:
+            paths_to_plots (dict):  dictionary with keys sample_id and value
+                path to empty file.
+        """
+        # Generate random images
+        paths_to_plots = {}
+
+        # Path to output
+        out_dir = os.path.join(tmp_path, "zip_this_dir")
+        os.makedirs(out_dir)
+
+        # Loop to create the empty files
+        for i in range(num_files):
+            # Specify the name of each empty file
+            file_name = f"empty_file_{i}.svg"
+
+            # Combine the directory path and file name to create the full
+            # file path
+            file_path = os.path.join(out_dir, file_name)
+            paths_to_plots[f"empty_file_{i}"] = file_path
+
+            # Create an empty file
+            with open(file_path, 'w'):
+                pass
+
+        return paths_to_plots
+
+    def test_zip_busco_plots_multiple(self):
+        """
+        Checks for existence of zip file.
+        """
+        with tempfile.TemporaryDirectory() as tmp_path:
+            paths_to_plots = self.mock_draw_busco_plots(
+                num_files=6, tmp_path=tmp_path
+            )
+
+            # Zip graphs for user download
+            zip_path = os.path.join(tmp_path, "busco_plots.zip")
+            _zip_busco_plots(paths_to_plots=paths_to_plots, zip_path=zip_path)
+
+            # Check for existence of file
+            self.assertTrue(os.path.exists(zip_path))
+
+    def test_zip_busco_plots_one(self):
+        """
+        Checks for existence of zip file.
+        """
+        with tempfile.TemporaryDirectory() as tmp_path:
+            paths_to_plots = self.mock_draw_busco_plots(
+                num_files=1, tmp_path=tmp_path
+            )
+
+            # Zip graphs for user download
+            zip_path = os.path.join(tmp_path, "busco_plots.zip")
+            _zip_busco_plots(paths_to_plots=paths_to_plots, zip_path=zip_path)
+
+            # Check for existence of file
+            self.assertTrue(os.path.exists(zip_path))
+
+    # Test `_run_busco`
+    def mock_run_busco(self, tmp_path, bins):
+        """
+        This function mimics the _run_busco function by creating the files
+        that are supposed to be generated. These files are empty.
+
+        Args:
+            tmp_path (str): path where to write empty files
+            bins (MultiMAGSequencesDirFmt): fixture from which the sample ids
+                are extracted.
+        """
+        # Get output path
+        output_dir = os.path.join(tmp_path, "busco_output")
+
+        # Creates pandas df "manifest" from bins
+        manifest: pd.DataFrame = bins.manifest.view(pd.DataFrame)
+
+        # numpy.ndarray with unique dirs
+        sample_ids = manifest.index.get_level_values(0).unique()
+
+        # write files to output
+        # Loop to create the empty files
+        expected = {}
+        for sample_id in sample_ids:
+            # Specify the name of each empty file
+            path_to_run_summary = os.path.join(
+                output_dir, sample_id, "batch_summary.txt"
+            )
+            os.makedirs(
+                os.path.dirname(path_to_run_summary),
+                exist_ok=True
+            )
+
+            # Save to dict
+            expected[sample_id] = path_to_run_summary
+
+            # Create an empty file
+            with open(path_to_run_summary, 'w'):
+                pass
+
+        # Return expected output
+        return expected
+
+    @patch('subprocess.run')
+    def test_run_busco(self, subp_run):
+        """
+        Test function `_run_busco`. Checks for dictionary equality.
+        """
+        with tempfile.TemporaryDirectory() as tmp_path:
+            # Define command arguments and parse them
+            fake_props = {"a": "b", "c": "d"}
+            expected = self.mock_run_busco(
+                tmp_path=tmp_path, bins=self.get_dummy_mags()
+            )
+
+            # Run busco and save paths to run summaries
+            observed = _run_busco(
+                output_dir=os.path.join(tmp_path, "busco_output"),
+                mags=self.get_dummy_mags(),
+                params=fake_props,
+            )
+
+            self.assertDictEqual(expected, observed)
+
+    @patch("subprocess.run")
+    def test_run_busco_exception(self, subp_run):
+        """
+        Test function `_run_busco`. Checks for a raised exception.
+        """
+        with tempfile.TemporaryDirectory() as tmp_path:
+            # Define command arguments and parse them
+            fake_props = {"a": "b", "c": "d"}
+
+            with self.assertRaises(FileNotFoundError):
+                # Run busco and save paths to run summaries
+                _ = _run_busco(
+                    output_dir=os.path.join(tmp_path, "busco_output"),
+                    mags=self.get_dummy_mags(),
+                    params=fake_props,
+                )
+
+    # Integration test busco.
+    @patch('q2_moshpit.busco.utils._run_busco')
+    @patch('q2_moshpit.busco.utils._zip_busco_plots')
+    @patch('q2_moshpit.busco.utils._draw_busco_plots')
+    @patch('q2_moshpit.busco.utils._collect_summaries_and_save')
+    def test_integration_busco(
+        self,
+        collect_summaries,
+        not_used_1,
+        not_used_2,
+        run_busco
+    ):
+        """
+        Tests entire busco run and patches the previously tested functions.
+        Checks for existence of HTML output.
+
+        Args:
+            collect_summaries (unittest.mock): mock object for function
+                `_collect_summaries_and_save`
+            not_used_1 (unittest.mock): mock object for function
+                `_draw_busco_plots`. Not used.
+            not_used_2 (unittest.mock): mock object for function
+                `_zip_busco_plots`. Not used.
+            run_busco (unittest.mock): mock object for function
+                `_run_busco`. 
+        """
+        with tempfile.TemporaryDirectory() as tmp_path:
+            # Define sid effects and return arguments for patches
+            # This side effect will return the path_to_run_summaries dict
+            run_busco.side_effect = self.mock_run_busco(
+                tmp_path=tmp_path,
+                bins=self.get_dummy_mags()
+            )
+
+            # This side effect will return the all_summaries_dfs
+            p = os.path.join(
+                os.path.dirname(__file__),
+                "data/all_batch_summaries.csv"
+            )
+            collect_summaries.return_value = pd.read_csv(p)
+
+            # Run busco
+            busco(output_dir=str(tmp_path), bins=self.get_dummy_mags())
+
+            # Check for the existence of the html file
+            self.assertTrue(os.path.exists(f"{tmp_path}/index.html"))

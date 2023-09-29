698e5698bc659323e1078c7dacb6d530e35ea233
Michal Ziemski <mziemski@ethz.ch>
2023-09-29 17:19:08 +0200

    ENH: allow passing MAGs to eggnog-diamond-search

--- q2_moshpit/eggnog/_method.py
+++ q2_moshpit/eggnog/_method.py
@@ -5,56 +5,67 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-
+import glob
 import subprocess
 import os
 import tempfile
-import re
+from typing import Union
+
 import pandas as pd
 
 from q2_types_genomics.per_sample_data import ContigSequencesDirFmt
 from q2_types_genomics.genome_data import SeedOrthologDirFmt, OrthologFileFmt
-from q2_types_genomics.feature_data import OrthologAnnotationDirFmt
+from q2_types_genomics.feature_data import OrthologAnnotationDirFmt, MAGSequencesDirFmt
 from q2_types_genomics.reference_db import EggnogRefDirFmt
 from q2_types.feature_data import DNAFASTAFormat
 from q2_types_genomics.reference_db import DiamondDatabaseDirFmt
 import qiime2.util
 
 
-def eggnog_diamond_search(input_sequences: ContigSequencesDirFmt,
-                          diamond_db: DiamondDatabaseDirFmt,
-                          num_cpus: int = 1, db_in_memory: bool = False
-                          ) -> (SeedOrthologDirFmt, pd.DataFrame):
+def eggnog_diamond_search(
+        sequences: Union[ContigSequencesDirFmt, MAGSequencesDirFmt],
+        diamond_db: DiamondDatabaseDirFmt,
+        num_cpus: int = 1, db_in_memory: bool = False
+) -> (SeedOrthologDirFmt, pd.DataFrame):
 
     diamond_db_fp = os.path.join(str(diamond_db), 'ref_db.dmnd')
     temp = tempfile.TemporaryDirectory()
 
     # run analysis
-    for relpath, obj_path in input_sequences.sequences.iter_views(
-            DNAFASTAFormat):
-        sample_label = str(relpath).rsplit(r'_', 1)[0]
-
-        _diamond_search_runner(input_path=obj_path,
-                               diamond_db=diamond_db_fp,
-                               sample_label=sample_label,
-                               output_loc=temp.name,
-                               num_cpus=num_cpus,
-                               db_in_memory=db_in_memory)
+    if isinstance(sequences, ContigSequencesDirFmt):
+        for relpath, obj_path in sequences.sequences.iter_views(
+                DNAFASTAFormat):
+            sample_id = str(relpath).rsplit(r'_', 1)[0]
+            _diamond_search_runner(
+                input_path=obj_path, diamond_db=diamond_db_fp,
+                sample_label=sample_id, output_loc=temp.name,
+                num_cpus=num_cpus, db_in_memory=db_in_memory
+            )
+    elif isinstance(sequences, MAGSequencesDirFmt):
+        for mag_fp in glob.glob(f'{sequences.path}/*.fa*'):
+            sample_id = os.path.splitext(os.path.basename(mag_fp))[0]
+            _diamond_search_runner(
+                input_path=mag_fp, diamond_db=diamond_db_fp,
+                sample_label=sample_id, output_loc=temp.name,
+                num_cpus=num_cpus, db_in_memory=db_in_memory
+            )
 
     result = SeedOrthologDirFmt()
-
-    for item in os.listdir(temp.name):
-        if re.match(r".*\.seed_orthologs", item):
-            qiime2.util.duplicate(os.path.join(temp.name, item),
-                                  os.path.join(result.path, item))
+    ortholog_fps = [
+        os.path.basename(x) for x
+        in glob.glob(f'{temp.name}/*.seed_orthologs')
+    ]
+    for item in ortholog_fps:
+        qiime2.util.duplicate(
+            os.path.join(temp.name, item), os.path.join(result.path, item)
+        )
 
     ft = _eggnog_feature_table(result)
 
-    return (result, ft)
+    return result, ft
 
 
 def _eggnog_feature_table(seed_orthologs: SeedOrthologDirFmt) -> pd.DataFrame:
-
     per_sample_counts = []
 
     for sample_path, obj in seed_orthologs.seed_orthologs.iter_views(

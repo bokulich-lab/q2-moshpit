88f9a0b61afda82cd5d2f8b2fdeac41633444334
Santiago Castro Dau <sanntiago5@gmail.com>
2023-11-13 15:42:28 +0100

    test_prodigal.py: assert exact calls

--- q2_moshpit/prodigal/tests/test_prodigal.py
+++ q2_moshpit/prodigal/tests/test_prodigal.py
@@ -6,10 +6,11 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+import os
 from q2_moshpit.prodigal.prodigal import predict_genes_prodigal
 from qiime2.plugin.testing import TestPluginBase
 from q2_types_genomics.feature_data import MAGSequencesDirFmt
-from unittest.mock import patch, ANY, call
+from unittest.mock import patch, call
 from q2_types_genomics.genome_data import (
     LociDirectoryFormat, GenesDirectoryFormat, ProteinsDirectoryFormat,
 )
@@ -30,17 +31,24 @@ class TestBUSCO(TestPluginBase):
         self.assertIsInstance(genes, GenesDirectoryFormat)
         self.assertIsInstance(proteins, ProteinsDirectoryFormat)
 
+        # Get names of fasta files from test data dir
+        fasta_file = [
+            os.path.splitext(file)[0] for file in os.listdir(mags.path)
+            if file.endswith(".fa") or file.endswith(".fasta")
+        ]
+
+        # Unpack list
+        fasta_file = fasta_file[0]
+
         # Assert that patch was called once
-        subp_run.assert_called_once_with(
-            [
-                "prodigal",
-                "-g", "11",
-                "-f", "gff",
-                "-i", ANY,
-                "-o", ANY,
-                "-a", ANY,
-                "-d", ANY
-            ],
+        subp_run.assert_called_once_with([
+            "prodigal",
+            "-g", "11",
+            "-f", "gff",
+            "-i", os.path.join(mags.path, f"{fasta_file}.fasta"),
+            "-o", os.path.join(loci.path, f"{fasta_file}_loci.gff"),
+            "-a", os.path.join(proteins.path, f"{fasta_file}_proteins.fasta"),
+            "-d", os.path.join(genes.path, f"{fasta_file}_genes.fasta")],
             check=True
         )
 
@@ -56,20 +64,23 @@ class TestBUSCO(TestPluginBase):
         self.assertIsInstance(genes, GenesDirectoryFormat)
         self.assertIsInstance(proteins, ProteinsDirectoryFormat)
 
+        # Get names of fasta files from test data dir
+        fasta_files = [
+            os.path.splitext(file)[0] for file in os.listdir(mags.path)
+            if file.endswith(".fa") or file.endswith(".fasta")
+        ]
+
         # Define calls
-        three_calls = [
-            call(
-                [
-                    "prodigal",
-                    "-g", "11",
-                    "-f", "gff",
-                    "-i", ANY,
-                    "-o", ANY,
-                    "-a", ANY,
-                    "-d", ANY
-                ],
-                check=True
-            ) for i in range(3)
+        three_calls = [call([
+            "prodigal",
+            "-g", "11",
+            "-f", "gff",
+            "-i", os.path.join(mags.path, f"{fasta_file}.fasta"),
+            "-o", os.path.join(loci.path, f"{fasta_file}_loci.gff"),
+            "-a", os.path.join(proteins.path, f"{fasta_file}_proteins.fasta"),
+            "-d", os.path.join(genes.path, f"{fasta_file}_genes.fasta")],
+            check=True)
+            for fasta_file in fasta_files
         ]
 
         # Assert that patch was called 3 times

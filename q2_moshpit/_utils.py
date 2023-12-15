22b2e313c67a97621fa30f4a4cdc958e42bc2e50
Santiago Castro Dau <sanntiago5@gmail.com>
2023-12-15 11:34:17 +0100

    Reverting the **kwargs in run_command

--- q2_moshpit/_utils.py
+++ q2_moshpit/_utils.py
@@ -9,7 +9,7 @@ import subprocess
 from typing import List
 
 
-def run_command(cmd, env=None, verbose=True, pipe=False, **kwargs):
+def run_command(cmd, env=None, verbose=True, pipe=False):
     if verbose:
         print("Running external command line application(s). This may print "
               "messages to stdout and/or stderr.")
@@ -26,9 +26,9 @@ def run_command(cmd, env=None, verbose=True, pipe=False, **kwargs):
         return result
 
     if env:
-        subprocess.run(cmd, env=env, check=True, **kwargs)
+        subprocess.run(cmd, env=env, check=True)
     else:
-        subprocess.run(cmd, check=True, **kwargs)
+        subprocess.run(cmd, check=True)
 
 
 def _construct_param(arg_name):

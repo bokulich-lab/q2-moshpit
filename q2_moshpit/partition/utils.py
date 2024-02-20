3db1fafa6cc85aea90f01a48f470758d6ff5e2d3
Santiago Castro Dau <sanntiago5@gmail.com>
2024-02-20 10:39:32 +0100

    adjust validation function

--- q2_moshpit/partition/utils.py
+++ q2_moshpit/partition/utils.py
@@ -9,8 +9,9 @@ import warnings
 
 
 def _validate_num_partitions(num_mags, num_partitions) -> int:
+
     if num_partitions is None:
-        num_partitions = num_mags
+        return num_mags
     elif num_partitions > num_mags:
         warnings.warn(
             "You have requested a number of partitions"
@@ -19,11 +20,13 @@ def _validate_num_partitions(num_mags, num_partitions) -> int:
             f" partitioned by MAG into '{num_mags}'"
             " partitions."
         )
-        num_partitions = num_mags
-    return num_partitions
+        return num_mags
+    else:
+        return num_partitions
 
 
 def _validate_mag_ids(num_partitions, num_mags, mags_all):
+
     if num_partitions == num_mags:
         mag_ids = [mag_id[1] for mag_id in mags_all]
         duplicates = [

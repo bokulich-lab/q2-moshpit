f001315fc240a435e81a97c233e2cf982a10d145
Santiago Castro Dau <sanntiago5@gmail.com>
2024-02-21 11:24:05 +0100

    missing files from update

--- q2_moshpit/plugin_setup.py
+++ q2_moshpit/plugin_setup.py
@@ -707,6 +707,23 @@ plugin.methods.register_function(
     citations=[citations["huerta_cepas_eggnog_2019"]]
 )
 
+plugin.methods.register_function(
+    function=q2_moshpit.partition.collate_annotations,
+    inputs={'ortholog_annotations': List[FeatureData[NOG]]},
+    parameters={},
+    outputs=[('collated_annotations', FeatureData[NOG])],
+    input_descriptions={
+        'ortholog_annotations': "Collection of orthologs annotations"
+    },
+    output_descriptions={
+        'collated_annotations': "Collated orthologs annotations"
+    },
+    name='Collate orthologs annotations',
+    description="Collate orthologs annotations from the eggnog-annotate "
+                "action. Intended when parallel runs of eggnog-diamond-search "
+                "and eggnog-annotate were used.",
+)
+
 busco_params = {
     "mode": Str % Choices(["genome"]),
     "lineage_dataset": Str,

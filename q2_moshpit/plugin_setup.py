18951511eafc4d5a3833af3a94607cdd9903b207
Santiago Castro Dau <sanntiago5@gmail.com>
2024-06-25 14:00:20 +0200

    seed-alignments to seed_alignments

--- q2_moshpit/plugin_setup.py
+++ q2_moshpit/plugin_setup.py
@@ -1479,14 +1479,14 @@ plugin.methods.register_function(
         ("idmap", EggnogHmmerIdmap % Properties("eggnog")),
         ("hmm_db", ProfileHMM[MultipleProtein] % Properties("eggnog")),
         ("pressed_hmm_db", ProfileHMM[PressedProtein] % Properties("eggnog")),
-        ("seed-alignments", GenomeData[Proteins] % Properties("eggnog"))
+        ("seed_alignments", GenomeData[Proteins] % Properties("eggnog"))
     ],
     output_descriptions={
         "idmap": "List of protein families in `hmm_db`.",
         "hmm_db": "Collection of Profile HMMs.",
         "pressed_hmm_db": "Collection of Profile HMMs in binary format "
                           "and indexed.",
-        "seed-alignments": "Seed alignments for the protein families in "
+        "seed_alignments": "Seed alignments for the protein families in "
                            "`hmm_db`."
     },
     name="Fetch the taxon specific database necessary to run the "

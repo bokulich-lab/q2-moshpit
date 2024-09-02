011fce06b3dd03e4d4c066d8851d50a2c6ece5bc
Michal Ziemski <mziemski@ethz.ch>
2024-09-02 15:33:27 +0200

    Add tests

--- q2_moshpit/kaiju/tests/test_classification.py
+++ q2_moshpit/kaiju/tests/test_classification.py
@@ -9,19 +9,21 @@ import json
 import os
 import unittest
 from subprocess import CalledProcessError
-from unittest.mock import patch, Mock, ANY
+from unittest.mock import patch, Mock, ANY, MagicMock, call
 
 import numpy as np
 import pandas as pd
+import qiime2
 from pandas._testing import assert_frame_equal
 from q2_types.per_sample_sequences import (
     SingleLanePerSamplePairedEndFastqDirFmt,
-    SingleLanePerSampleSingleEndFastqDirFmt
+    SingleLanePerSampleSingleEndFastqDirFmt,
+    CasavaOneEightSingleLanePerSampleDirFmt
 )
 
 from q2_moshpit.kaiju.classification import (
     _construct_feature_table, _rename_taxon, _encode_unclassified_ids,
-    _fix_id_types, _process_kaiju_reports, _classify_kaiju
+    _fix_id_types, _process_kaiju_reports, _classify_kaiju, classify_kaiju
 )
 from qiime2.plugin.testing import TestPluginBase
 from qiime2.plugins import moshpit
@@ -32,6 +34,27 @@ class TestKaijuClassification(TestPluginBase):
 
     def setUp(self):
         super().setUp()
+        self.ctx = MagicMock()
+
+        self.mock_classify_kaiju = MagicMock(
+            side_effect=[("table1", "taxonomy1"), ("table2", "taxonomy2")]
+        )
+        self.ctx.get_action.side_effect = lambda domain, action_name: {
+            ('moshpit', '_classify_kaiju'): self.mock_classify_kaiju,
+            ('feature_table', 'merge'): self.mock_merge,
+            ('feature_table', 'merge_taxa'): self.mock_merge_taxa,
+            ('demux', 'partition_samples_single'): self.mock_partition_single,
+            ('demux', 'partition_samples_paired'): self.mock_partition_paired,
+        }[domain, action_name]
+
+        # Additional action mocks
+        self.mock_merge = MagicMock(return_value=("merged_table",))
+        self.mock_merge_taxa = MagicMock(return_value=("merged_taxa",))
+        self.mock_partition_single = MagicMock()
+        self.mock_partition_paired = MagicMock()
+
+        self.mock_partition_single.return_value = {'part1': Mock()}
+        self.mock_partition_paired.return_value = {'part1': Mock()}
         self.classify_kaiju = moshpit.pipelines.classify_kaiju
         with open(self.get_data_path('taxa-map.json')) as f:
             self.taxa_map = json.load(f)
@@ -264,6 +287,149 @@ class TestKaijuClassification(TestPluginBase):
                 r="class", c=0.1, exp=True, u=True
             )
 
+    def test_classify_kaiju_single_partition_single_end(self):
+        fake_seqs = qiime2.Artifact.import_data(
+            "SampleData[SequencesWithQuality]",
+            self.get_data_path("single-end-casava"),
+            CasavaOneEightSingleLanePerSampleDirFmt
+        )
+        fake_db = Mock()
+
+        self.mock_partition_single.side_effect = [({0: "part1"},)]
+
+        out_table, out_taxonomy = classify_kaiju(
+            self.ctx, fake_seqs, fake_db, num_partitions=1
+        )
+
+        self.ctx.get_action.assert_any_call("moshpit", "_classify_kaiju")
+        self.ctx.get_action.assert_any_call("feature_table", "merge")
+        self.ctx.get_action.assert_any_call("feature_table", "merge_taxa")
+        self.ctx.get_action.assert_any_call(
+            "demux", "partition_samples_single"
+        )
+
+        self.mock_partition_single.assert_called_once_with(fake_seqs, 1)
+        self.mock_partition_paired.assert_not_called()
+        self.mock_classify_kaiju.assert_called_once_with(
+            "part1", fake_db, z=1, a='greedy', e=3, m=11, s=65, evalue=0.01,
+            x=True, r='species', c=0.0, exp=False, u=False
+        )
+        self.mock_merge.assert_called_once_with(["table1"])
+        self.mock_merge_taxa.assert_called_once_with(["taxonomy1"])
+        self.assertEqual("merged_table", out_table)
+        self.assertEqual("merged_taxa", out_taxonomy)
+
+    def test_classify_kaiju_single_partition_paired_end(self):
+        fake_seqs = qiime2.Artifact.import_data(
+            "SampleData[PairedEndSequencesWithQuality]",
+            self.get_data_path("paired-end-casava"),
+            CasavaOneEightSingleLanePerSampleDirFmt
+        )
+        fake_db = Mock()
+
+        self.mock_partition_paired.side_effect = [({0: "part1"},)]
+
+        out_table, out_taxonomy = classify_kaiju(
+            self.ctx, fake_seqs, fake_db, num_partitions=1
+        )
+
+        self.ctx.get_action.assert_any_call("moshpit", "_classify_kaiju")
+        self.ctx.get_action.assert_any_call("feature_table", "merge")
+        self.ctx.get_action.assert_any_call("feature_table", "merge_taxa")
+        self.ctx.get_action.assert_any_call(
+            "demux", "partition_samples_paired"
+        )
+
+        self.mock_partition_single.assert_not_called()
+        self.mock_partition_paired.assert_called_once_with(fake_seqs, 1)
+        self.mock_classify_kaiju.assert_called_once_with(
+            "part1", fake_db, z=1, a='greedy', e=3, m=11, s=65, evalue=0.01,
+            x=True, r='species', c=0.0, exp=False, u=False
+        )
+        self.mock_merge.assert_called_once_with(["table1"])
+        self.mock_merge_taxa.assert_called_once_with(["taxonomy1"])
+        self.assertEqual("merged_table", out_table)
+        self.assertEqual("merged_taxa", out_taxonomy)
+
+    def test_classify_kaiju_multiple_partitions_single_end(self):
+        fake_seqs = qiime2.Artifact.import_data(
+            "SampleData[SequencesWithQuality]",
+            self.get_data_path("single-end-casava"),
+            CasavaOneEightSingleLanePerSampleDirFmt
+        )
+        fake_db = Mock()
+
+        self.mock_partition_single.side_effect = [({0: "part1", 1: "part2"},)]
+
+        out_table, out_taxonomy = classify_kaiju(
+            self.ctx, fake_seqs, fake_db, num_partitions=2
+        )
+
+        self.ctx.get_action.assert_any_call("moshpit", "_classify_kaiju")
+        self.ctx.get_action.assert_any_call("feature_table", "merge")
+        self.ctx.get_action.assert_any_call("feature_table", "merge_taxa")
+        self.ctx.get_action.assert_any_call(
+            "demux", "partition_samples_single"
+        )
+
+        self.mock_partition_single.assert_called_once_with(fake_seqs, 2)
+        self.mock_partition_paired.assert_not_called()
+        self.mock_classify_kaiju.assert_has_calls([
+            call(
+                "part1", fake_db, z=1, a='greedy', e=3, m=11, s=65,
+                evalue=0.01, x=True, r='species', c=0.0, exp=False, u=False
+            ),
+            call(
+                "part2", fake_db, z=1, a='greedy', e=3, m=11, s=65,
+                evalue=0.01, x=True, r='species', c=0.0, exp=False, u=False
+            )
+        ])
+        self.mock_merge.assert_called_once_with(["table1", "table2"])
+        self.mock_merge_taxa.assert_called_once_with(
+            ["taxonomy1", "taxonomy2"]
+        )
+        self.assertEqual("merged_table", out_table)
+        self.assertEqual("merged_taxa", out_taxonomy)
+
+    def test_classify_kaiju_multiple_partitions_paired_end(self):
+        fake_seqs = qiime2.Artifact.import_data(
+            "SampleData[PairedEndSequencesWithQuality]",
+            self.get_data_path("paired-end-casava"),
+            CasavaOneEightSingleLanePerSampleDirFmt
+        )
+        fake_db = Mock()
+
+        self.mock_partition_paired.side_effect = [({0: "part1", 1: "part2"},)]
+
+        out_table, out_taxonomy = classify_kaiju(
+            self.ctx, fake_seqs, fake_db, num_partitions=2
+        )
+
+        self.ctx.get_action.assert_any_call("moshpit", "_classify_kaiju")
+        self.ctx.get_action.assert_any_call("feature_table", "merge")
+        self.ctx.get_action.assert_any_call("feature_table", "merge_taxa")
+        self.ctx.get_action.assert_any_call(
+            "demux", "partition_samples_paired"
+        )
+
+        self.mock_partition_single.assert_not_called()
+        self.mock_partition_paired.assert_called_once_with(fake_seqs, 2)
+        self.mock_classify_kaiju.assert_has_calls([
+            call(
+                "part1", fake_db, z=1, a='greedy', e=3, m=11, s=65,
+                evalue=0.01, x=True, r='species', c=0.0, exp=False, u=False
+            ),
+            call(
+                "part2", fake_db, z=1, a='greedy', e=3, m=11, s=65,
+                evalue=0.01, x=True, r='species', c=0.0, exp=False, u=False
+            )
+        ])
+        self.mock_merge.assert_called_once_with(["table1", "table2"])
+        self.mock_merge_taxa.assert_called_once_with(
+            ["taxonomy1", "taxonomy2"]
+        )
+        self.assertEqual("merged_table", out_table)
+        self.assertEqual("merged_taxa", out_taxonomy)
 
 if __name__ == "__main__":
     unittest.main()
